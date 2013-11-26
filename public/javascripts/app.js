c9712a8b9a079b714c5725398af702ce330b4196
root <root@18.0.18.175.adsl-pool.jlccptt.net.cn>
2013-11-26 09:31:58 -0500

    Code for geolocation

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,15 +3,38 @@ app.maps = {};
 
 $(document).ready(function(){
 	
-	app.maps.initialize();
+	app.maps.initGeolocation();
 
 });
 
-app.maps.initialize = function(){
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
+app.maps.initGeolocation = function()
+{
+     if( navigator.geolocation )
+     {
+          // Call getCurrentPosition with success and failure callbacks
+          navigator.geolocation.getCurrentPosition( app.maps.geolocationSuccesCallback, app.maps.geolocationFailCallback );
+     }
+     else
+     {
+          alert("Sorry, your browser does not support geolocation services.");
+     }
+};
+ 
+app.maps.geolocationSuccesCallback = function(position)
+{
+ 	app.maps.createMap(position.coords.latitude,position.coords.longitude);
+}
+app.maps.geolocationFailCallback= function()
+{
+     //set deault values
+     app.maps.createMap(42.345573,-71.098326);
+};
 
-  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+app.maps.createMap = function(latitude,longitude){
+	var mapOptions = {
+    	zoom: 15,
+    	center: new google.maps.LatLng(latitude,longitude)
+  	};
+
+	var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 };
---
+++ public/javascripts/config.js
@@ -0,0 +1,3 @@
+var config = config || {};
+config.init.latitude = -71.098326;
+config.init.longitude = 42.345573;
